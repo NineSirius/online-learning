dd15b494a68c74c6f9c2fe82e740c627a606c7b3
Ruslan <ruslanbulah3@gmail.com>
2022-12-15 12:22:56 +0600

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,212 +1,229 @@
-const hamburger = document.querySelector('.hamburger');
-const nav = document.querySelector('.nav');
-const overlay = document.querySelector('.overlay');
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".nav");
+const overlay = document.querySelector(".overlay");
 
-const modalAuthbtn = document.querySelector('.nav-auth-btn');
-const modalAuthbox = document.querySelector('.modalbox-auth');
-const modalAuth = document.querySelector('.modal');
-const closeBtn = document.querySelector('.closebtn');
-const submitbtn = document.querySelector('.modal-btn')
+const modalAuthbtn = document.querySelector(".nav-auth-btn");
+const modalAuthbox = document.querySelector(".modalbox-auth");
+const modalAuth = document.querySelector(".modal");
+const closeBtn = document.querySelector(".closebtn");
+const submitbtn = document.querySelector(".modal-btn");
 
-const inputTitle = document.querySelector('.input-title');
-const input = document.querySelector('.auth-input');
-const inputTitle2 = document.querySelector('.input-title2');
-const input2 = document.querySelector('.auth-input2');
+const inputTitle = document.querySelector(".input-title");
+const input = document.querySelector(".auth-input");
+const inputTitle2 = document.querySelector(".input-title2");
+const input2 = document.querySelector(".auth-input2");
 
-const nextbtn = document.querySelector('.slider-next-btn');
-const prevbtn = document.querySelector('.slider-prev-btn');
-const slider1 = document.querySelector('.slider-item1')
-const slider2 = document.querySelector('.slider-item2')
-const slider3 = document.querySelector('.slider-item3')
-const slider4 = document.querySelector('.slider-item4')
+const nextbtn = document.querySelector(".slider-next-btn");
+const prevbtn = document.querySelector(".slider-prev-btn");
+const slider1 = document.querySelector(".slider-item1");
+const slider2 = document.querySelector(".slider-item2");
+const slider3 = document.querySelector(".slider-item3");
+const slider4 = document.querySelector(".slider-item4");
 
-const modalSisAdminbtn = document.querySelector('.modal-sis-admin');
-const modalSisAdminBox = document.querySelector('.modalbox-course3');
-const modalSisAdmin = document.querySelector('.modal-course3');
-const closeBtn2 = document.querySelector('.closebtn-course3')
+const modalSisAdminbtn = document.querySelector(".modal-sis-admin");
+const modalSisAdminBox = document.querySelector(".modalbox-course3");
+const modalSisAdmin = document.querySelector(".modal-course3");
+const closeBtn2 = document.querySelector(".closebtn-course3");
 
-const modalWebDevBtn = document.querySelector('.modal-front-end');
-const modalcourseBox = document.querySelector('.modalbox-course');
-const modalCourse = document.querySelector('.modal-course');
-const closebtnCourse = document.querySelector('.closebtn-course')
+const modalWebDevBtn = document.querySelector(".modal-front-end");
+const modalcourseBox = document.querySelector(".modalbox-course");
+const modalCourse = document.querySelector(".modal-course");
+const closebtnCourse = document.querySelector(".closebtn-course");
 
-const navLink = document.querySelector('.nav-link');
-const navLink2 = document.querySelector('.nav-link2');
-const navLink3 = document.querySelector('.nav-link3');
-const navLink4 = document.querySelector('.nav-link4');
+const navLink = document.querySelectorAll(".nav-link");
+// const navLink2 = document.querySelector(".nav-link2");
+// const navLink3 = document.querySelector(".nav-link3");
+// const navLink4 = document.querySelector(".nav-link4");
 
 let x;
-addEventListener('touchstart', e => x = e.changedTouches[0].clientX);
+addEventListener("touchstart", (e) => (x = e.changedTouches[0].clientX));
 
-addEventListener('touchend', e => e.changedTouches[0].clientX - x < -200 && swipeLeft());
+addEventListener(
+    "touchend",
+    (e) => e.changedTouches[0].clientX - x < -200 && swipeLeft()
+);
+
 
 function swipeLeft() {
-  hamburger.classList.add('is-active');
-  nav.classList.add('is-active');
-  overlay.classList.add('is-active');
-  document.body.classList.add('noscroll');
+    hamburger.classList.add("is-active");
+    nav.classList.add("is-active");
+    overlay.classList.add("is-active");
+    document.body.classList.add("noscroll");
 }
 
-hamburger.addEventListener('click', function(){
-  hamburger.classList.toggle('is-active');
-  nav.classList.toggle('is-active');
-  overlay.classList.toggle('is-active');
-  document.body.classList.toggle('noscroll');
+    
+hamburger.addEventListener("click", function () {
+    hamburger.classList.toggle("is-active");
+    nav.classList.toggle("is-active");
+    overlay.classList.toggle("is-active");
+    document.body.classList.toggle("noscroll");
 });
 
-overlay.addEventListener('click', function(){
-  hamburger.classList.remove('is-active');
-  nav.classList.remove('is-active');
-  overlay.classList.remove('is-active');
-  document.body.classList.remove('noscroll');
+overlay.addEventListener("click", function () {
+    hamburger.classList.remove("is-active");
+    nav.classList.remove("is-active");
+    overlay.classList.remove("is-active");
+    document.body.classList.remove("noscroll");
 });
 
-navLink.addEventListener('click', function(){
-  hamburger.classList.remove('is-active');
-  nav.classList.remove('is-active');
-  overlay.classList.remove('is-active');
-  document.body.classList.remove('noscroll');
-});
-navLink2.addEventListener('click', function(){
-  hamburger.classList.remove('is-active');
-  nav.classList.remove('is-active');
-  overlay.classList.remove('is-active');
-  document.body.classList.remove('noscroll');
-});
+for (let i = 0; i < navLink.length; i++) {
+    navLink[i].addEventListener("click", function () {
+        hamburger.classList.remove("is-active");
+        nav.classList.remove("is-active");
+        overlay.classList.remove("is-active");
+        document.body.classList.remove("noscroll");
+    });
+}
 
-navLink3.addEventListener('click', function(){
-  hamburger.classList.remove('is-active');
-  nav.classList.remove('is-active');
-  overlay.classList.remove('is-active');
-  document.body.classList.remove('noscroll');
+modalAuthbtn.addEventListener("click", function () {
+    modalAuthbox.classList.toggle("active");
+    modalAuth.classList.toggle("active");
 });
 
-navLink4.addEventListener('click', function(){
-  hamburger.classList.remove('is-active');
-  nav.classList.remove('is-active');
-  overlay.classList.remove('is-active');
-  document.body.classList.remove('noscroll');
+modalAuthbox.addEventListener("click", function (e) {
+    if (e.target.classList.contains("modalbox")) {
+        modalAuthbox.classList.remove("active");
+        modalAuth.classList.remove("active");
+    }
 });
 
-modalAuthbtn.addEventListener('click', function(){
-  modalAuthbox.classList.toggle('active');
-  modalAuth.classList.toggle('active');
+closeBtn.addEventListener("click", function () {
+    modalAuthbox.classList.remove("active");
+    modalAuth.classList.remove("active");
 });
 
-modalAuthbox.addEventListener('click', function(e){
-  if(e.target.classList.contains('modalbox')) {
-    modalAuthbox.classList.remove('active')
-    modalAuth.classList.remove('active');
-  }
+input.addEventListener("click", function () {
+    if (inputTitle.classList.contains("show")) {
+        inputTitle2.classList.remove("show");
+    } else {
+        inputTitle.classList.toggle("show");
+        inputTitle2.classList.remove("show");
+    }
 });
 
-closeBtn.addEventListener('click', function(){
-  modalAuthbox.classList.remove('active')
-  modalAuth.classList.remove('active');
+input2.addEventListener("click", function () {
+    if (inputTitle2.classList.contains("show")) {
+        inputTitle.classList.remove("show");
+    } else {
+        inputTitle2.classList.toggle("show");
+        inputTitle.classList.remove("show");
+    }
 });
 
-
-input.addEventListener('click', function(){
-  if(inputTitle.classList.contains('show')){
-    inputTitle2.classList.remove('show');
-  } else {
-    inputTitle.classList.toggle('show');
-    inputTitle2.classList.remove('show')
-  }
+nextbtn.addEventListener("click", function () {
+    if (
+        slider2.classList.contains("hide") &&
+        slider3.classList.contains("hide") &&
+        slider4.classList.contains("hide")
+    ) {
+        slider2.classList.remove("hide");
+        slider1.classList.add("hide");
+    } else if (
+        slider1.classList.contains("hide") &&
+        slider3.classList.contains("hide") &&
+        slider4.classList.contains("hide")
+    ) {
+        slider2.classList.add("hide");
+        slider3.classList.remove("hide");
+    } else if (
+        slider1.classList.contains("hide") &&
+        slider2.classList.contains("hide") &&
+        slider4.classList.contains("hide")
+    ) {
+        slider3.classList.add("hide");
+        slider4.classList.remove("hide");
+    } else if (
+        slider1.classList.contains("hide") &&
+        slider2.classList.contains("hide") &&
+        slider3.classList.contains("hide")
+    ) {
+        slider4.classList.add("hide");
+        slider1.classList.remove("hide");
+    }
 });
 
-input2.addEventListener('click', function(){
-  if(inputTitle2.classList.contains('show')){
-    inputTitle.classList.remove('show');
-  } else {
-    inputTitle2.classList.toggle('show');
-    inputTitle.classList.remove('show');
-  }
+prevbtn.addEventListener("click", function () {
+    if (
+        slider3.classList.contains("hide") &&
+        slider2.classList.contains("hide") &&
+        slider1.classList.contains("hide")
+    ) {
+        slider3.classList.remove("hide");
+        slider4.classList.add("hide");
+    } else if (
+        slider1.classList.contains("hide") &&
+        slider2.classList.contains("hide") &&
+        slider4.classList.contains("hide")
+    ) {
+        slider3.classList.add("hide");
+        slider2.classList.remove("hide");
+    } else if (
+        slider1.classList.contains("hide") &&
+        slider3.classList.contains("hide") &&
+        slider4.classList.contains("hide")
+    ) {
+        slider2.classList.add("hide");
+        slider1.classList.remove("hide");
+    } else if (
+        slider2.classList.contains("hide") &&
+        slider3.classList.contains("hide") &&
+        slider4.classList.contains("hide")
+    ) {
+        slider1.classList.add("hide");
+        slider4.classList.remove("hide");
+    }
+    
 });
 
+AudioObj = document.querySelector(".audio");
+if (!slider4.classList.contains("hide")) {
+    AudioObj.play();        
+} else {
+    AudioObj.load();
+}
 
+modalSisAdminbtn.addEventListener("click", function () {
+    modalSisAdminBox.classList.toggle("active");
+    modalSisAdmin.classList.toggle("active");
+});
 
-nextbtn.addEventListener('click', function(){ 
-  if(slider2.classList.contains('hide') && slider3.classList.contains('hide') && slider4.classList.contains('hide') ){
-    slider2.classList.remove('hide');
-    slider1.classList.add('hide');
+modalSisAdminBox.addEventListener("click", function (e) {
+    if (e.target.classList.contains("modalbox-course3")) {
+        modalSisAdminBox.classList.remove("active");
+        modalSisAdmin.classList.remove("active");
+    }
+});
 
-  } else if(slider1.classList.contains('hide') && slider3.classList.contains('hide') && slider4.classList.contains('hide')) {
-    slider2.classList.add('hide')
-    slider3.classList.remove('hide');
-  } else if(slider1.classList.contains('hide') && slider2.classList.contains('hide') && slider4.classList.contains('hide')){
-    slider3.classList.add('hide');
-    slider4.classList.remove('hide');
-  } else if(slider1.classList.contains('hide') && slider2.classList.contains('hide') && slider3.classList.contains('hide')){
-    slider4.classList.add('hide');
-    slider1.classList.remove('hide');
-  }
-})
+closeBtn2.addEventListener("click", function (e) {
+    modalSisAdminBox.classList.remove("active");
+    modalSisAdmin.classList.remove("active");
+});
 
-prevbtn.addEventListener('click', function(){
-  if(slider3.classList.contains('hide') && slider2.classList.contains('hide') && slider1.classList.contains('hide') ){
-    slider3.classList.remove('hide');
-    slider4.classList.add('hide');
+modalWebDevBtn.addEventListener("click", function () {
+    modalcourseBox.classList.toggle("active");
+    modalCourse.classList.toggle("active");
+});
 
-  } else if(slider1.classList.contains('hide') && slider2.classList.contains('hide') && slider4.classList.contains('hide')) {
-    slider3.classList.add('hide')
-    slider2.classList.remove('hide');
-  } else if(slider1.classList.contains('hide') && slider3.classList.contains('hide') && slider4.classList.contains('hide')){
-    slider2.classList.add('hide');
-    slider1.classList.remove('hide');
-  } else if(slider2.classList.contains('hide') && slider3.classList.contains('hide') && slider4.classList.contains('hide')){
-    slider1.classList.add('hide');
-    slider4.classList.remove('hide');
-  }
-})
+modalcourseBox.addEventListener("click", function (e) {
+    if (e.target.classList.contains("modalbox-course")) {
+        modalcourseBox.classList.remove("active");
+        modalCourse.classList.remove("active");
+    }
+});
 
-modalSisAdminbtn.addEventListener('click', function(){
-  modalSisAdminBox.classList.toggle('active');
-  modalSisAdmin.classList.toggle('active');
-})
+closebtnCourse.addEventListener("click", function (e) {
+    modalcourseBox.classList.remove("active");
+    modalCourse.classList.remove("active");
+});
 
-modalSisAdminBox.addEventListener('click', function(e){
-  if(e.target.classList.contains('modalbox-course3')) {
-    modalSisAdminBox.classList.remove('active')
-    modalSisAdmin.classList.remove('active');
-  }
-})
+const topButton = document.querySelector(".top-btn");
 
-closeBtn2.addEventListener('click', function(e){
-    modalSisAdminBox.classList.remove('active')
-    modalSisAdmin.classList.remove('active');
-})
+window.addEventListener("scroll", function () {
+    wScroll = this.scrollY;
 
-modalWebDevBtn.addEventListener('click', function(){
-  modalcourseBox.classList.toggle('active');
-  modalCourse.classList.toggle('active');
-})
-
-modalcourseBox.addEventListener('click', function(e){
-  if(e.target.classList.contains('modalbox-course')) {
-    modalcourseBox.classList.remove('active')
-    modalCourse.classList.remove('active');
-  }
-})
-
-closebtnCourse.addEventListener('click', function(e){
-    modalcourseBox.classList.remove('active')
-    modalCourse.classList.remove('active');
-})
-
-const topButton = document.querySelector('.top-btn');
-
-window.addEventListener('scroll', function(){
-  wScroll = this.scrollY;
-
-  if(wScroll >= 500) {
-    topButton.classList.add('active');
-  } else {
-    topButton.classList.remove('active');
-  }
-})  
-
-
-
-
-
+    if (wScroll >= 500) {
+        topButton.classList.add("active");
+    } else {
+        topButton.classList.remove("active");
+    }
+});
